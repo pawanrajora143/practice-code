f725cf3b7ad7a1837164272fa3b88e4e2dfca24f
pawan rajora <pawangrajora143@gmail.com>
2024-03-21 22:48:17 +0530

    contact form

--- Html-Css-Project/website-2/index.js
+++ Html-Css-Project/website-2/index.js
@@ -17,18 +17,35 @@ navtoggle.addEventListener("click" , function(){
 
 // ===============code for Back to top button=============
 
-
 const backToTop = document.querySelector(".on-top")
 
 window.addEventListener("scroll" , ()=> {
-if(window.scrollY > 600){
-
-    backToTop.style.display="block"
-    
+if(window.scrollY >300){
+backToTop.style.display="block"
 }
 else{
+        backToTop.style.display="none"
+}
+})
 
-    backToTop.style.display="none"
+
+// ===============code header sticky button=============
+
+const header = document.querySelector("#header")
+const imgageChange1 = document.querySelector(".frst1")
+const imgageChange2 = document.querySelector(".frst2")
+
+window.addEventListener("scroll" , ()=> {
+if(window.scrollY >400){
+
+    header.classList.add("sticky")
+    imgageChange1.style.display="none"
+    imgageChange2.style.display="block"
+}
+else{
+    header.classList.remove("sticky")
+    imgageChange1.style.display="block"
+    imgageChange2.style.display="none"
 }
 
 })
